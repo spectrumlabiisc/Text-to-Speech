1b546f920470d57ab38ab0a4f7c9eb62bd47a27c
Harshwin <harshwin1693@gmail.com>
2016-08-17 15:17:48 +0530

    Add files via upload

---
+++ Straight_ttsdemo.sh
@@ -0,0 +1,77 @@
+AWK='mawk'
+#DUMPFEATS=$FESTDIR/examples/dumpfeats
+TEXT2UTT='/home/computing4/installations/festival/examples/text2utt';
+DUMPFEATS='/home/computing4/installations/festival/examples/dumpfeats';
+STRTDIR='/home/computing4/iisc_kannada_hts/M_STRAIGHT';
+BSTRTDIR='/home/computing4/iisc_kannada_hts'
+
+#voicedir='/home/spectrumlab/Desktop/installations/TSM/english_iisc_synth';
+#tsmdir='/home/spectrumlab/Desktop/installations/TSM/TSM_CODE_V3';
+#Generate utterance
+
+echo "Enter Text: "
+
+read input
+
+rm -rf $BSTRTDIR/Gen01.txt
+echo $input >> $BSTRTDIR/Gen01.txt
+T=$BSTRTDIR/Gen01.txt;
+sed -i 's/^/( Gen01 " /'  $BSTRTDIR/Gen01.txt 
+sed -i 's/$/ " )/'  $BSTRTDIR/Gen01.txt 
+cd $BSTRTDIR
+
+
+			if [ -s $T ]; then \
+				echo "*----------------------here-------------" 
+				festival -b festvox/build_clunits.scm '(build_prompts "Gen01.txt")'
+				
+				rm -f $STRTDIR/data/utts/*
+				cp prompt-utt/Gen01.utt $STRTDIR/data/utts
+				
+				rm -f $STRTDIR/data/scp/gen.scp 
+			fi
+cd $STRTDIR/data/    
+make testlab  			
+
+#$DUMPFEATS -eval scripts/extra_feats.scm -relation Segment -feats scripts/label.feats -output tmp.feats utts; \
+#			if [ -s tmp.feats ]; then \
+#				$AWK -f scripts/label-full.awk tmp.feats > labels/gen/Gen01.lab; \
+#				#$AWK -f scripts/label-mono.awk tmp.feats > labels/mono/Gen01.lab; \
+#				#$AWK -f scripts/label-mono.awk tmp.feats > labels/full/Gen01.lab; \	
+#				rm -f tmp.feats; \
+#			fi; \
+#rm -f $STRTDIR/data/scp/gen.scp 
+echo $STRTDIR/data/labels/gen/Gen01.lab >> $STRTDIR/data/scp/gen.scp
+
+
+
+make testlist
+#cat lists/full.list > tmp
+#for lab in labels/gen/*.lab; do \
+#	sed -e "s/.* //g" $lab >> tmp; \
+#done
+#sort -u tmp > lists/full_all.list
+#rm -f tmp
+
+# Generating a monophone list file
+#for lab in labels/mono/$(DATASET)_$(SPEAKER)_*.lab; do \
+#	if [ -s $lab -a -s labels/full/`basename $lab` -a -s cmp/`basename $lab .lab`.cmp ]; then \
+#		sed -e "s/.* //g" $lab >> tmp; \
+#	fi \
+#done	
+#sort -u tmp > lists/mono.list
+#rm -f tmp
+
+
+#synthesize voice
+
+cd $STRTDIR
+
+make voice
+sleep 20
+#play waveform
+
+#play $STRTDIR/gen/qst001/ver1/1mix/0/Gen01.wav
+
+
+
